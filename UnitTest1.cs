3df22b8e39301fdf543fc820608c224bc333e77f
rajkumar200059 <rajkumar200059@gmail.com>
2023-11-24 12:20:35 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver("C:\\Users\\rajku\\source\\repos\\TestProject1\\bin\\Debug\\net6.0\\drivers\\chromedriver.exe");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://www.google.com/";
+
+            // we can write code to find the elements using XPath
+
+
+            driver.Quit();
+        }
+    }
+}
